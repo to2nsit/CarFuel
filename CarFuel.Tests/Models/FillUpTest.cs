d6d35563f5f09357d0eea35b54fbb7bc3bf6b520
Pipat Wangmoon <pipat.wangmoon@gmail.com>
2016-09-01 16:12:06 +0700

    Add CarFuel Utilities for SystemTime
    And fixed #3 - Fill Up has Fill Up Date/Time.

--- CarFuel.Tests/Models/FillUpTest.cs
+++ CarFuel.Tests/Models/FillUpTest.cs
@@ -6,65 +6,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using CarFuel.Utilities;
 
 namespace CarFuel.Tests.Models {
-  public class FillUpTest {
+    public class FillUpTest {
 
-    public class KmLProperty { // inner or nested class
 
-      [Fact]
-      public void NewFillUpDontHasKmL() {
-        //arrange
-        var f1 = new FillUp();
+        public class General {
+            [Fact]
+            public void NewFillUp_HasFillUpDateAsNow() {
+                var f = new FillUp();
+                var now = DateTime.Now;
+                SystemTime.SetDateTime(now);
 
-        //act
-        double? kml = f1.KmL; // Nullable<double
+                var d = f.FillUpDate;
 
-        //assert
-        Assert.Null(kml);
-      }
+                Assert.Equal(now, d);
+            }
+        }
 
-      [Theory]
-      [InlineData(1000, 40.0, 2000, 50.0, 20.0)]
-      [InlineData(2000, 50.0, 2500, 20.0, 25.0)]
-      public void TwoFillUpsCanCalculateKmL(int odo1, double liters1, int odo2, double liters2, double expectedKmL) {
+        public class KmLProperty { // inner or nested class
 
-        var f1 = new FillUp(odo1, liters1);
-        var f2 = new FillUp(odo2, liters2);
+            [Fact]
+            public void NewFillUpDontHasKmL() {
+                //arrange
+                var f1 = new FillUp();
 
-        f1.NextFillUp = f2;
+                //act
+                double? kml = f1.KmL; // Nullable<double
 
-        var kml1 = f1.KmL;
-        var kml2 = f2.KmL;
+                //assert
+                Assert.Null(kml);
+            }
 
-        Assert.Equal(expectedKmL, kml1);
-        Assert.Null(kml2);
+            [Theory]
+            [InlineData(1000, 40.0, 2000, 50.0, 20.0)]
+            [InlineData(2000, 50.0, 2500, 20.0, 25.0)]
+            public void TwoFillUpsCanCalculateKmL(int odo1, double liters1, int odo2, double liters2, double expectedKmL) {
 
-      }
-      //[Fact]
-      //public void TwoFillUpsCanCalculateKmL_2() {
-      //  var f1 = new FillUp(2000, 50.0);
-      //  var f2 = new FillUp(2500, 20.0);
-      //  f1.NextFillUp = f2;
-      //  var kml1 = f1.KmL;
-      //  Assert.Equal(25.0, kml1);
-      //}
+                var f1 = new FillUp(odo1, liters1);
+                var f2 = new FillUp(odo2, liters2);
 
-      [Fact]
-      public void OdometerMustGreaterThanThePreviousFillUp() {
-        var f1 = new FillUp(50000, 50.0);
-        var f2 = new FillUp(49000, 60.0); //** invalid odo **
-        f1.NextFillUp = f2;
+                f1.NextFillUp = f2;
 
-        var ex = Assert.Throws<Exception>(() => {
+                var kml1 = f1.KmL;
+                var kml2 = f2.KmL;
 
-          var kml = f1.KmL;
+                Assert.Equal(expectedKmL, kml1);
+                Assert.Null(kml2);
 
-        });
+            }
+            //[Fact]
+            //public void TwoFillUpsCanCalculateKmL_2() {
+            //  var f1 = new FillUp(2000, 50.0);
+            //  var f2 = new FillUp(2500, 20.0);
+            //  f1.NextFillUp = f2;
+            //  var kml1 = f1.KmL;
+            //  Assert.Equal(25.0, kml1);
+            //}
 
-        ex.Message.ShouldEqual("Odometer Error");
-      } 
+            [Fact]
+            public void OdometerMustGreaterThanThePreviousFillUp() {
+                var f1 = new FillUp(50000, 50.0);
+                var f2 = new FillUp(49000, 60.0); //** invalid odo **
+                f1.NextFillUp = f2;
 
+                var ex = Assert.Throws<Exception>(() => {
+
+                    var kml = f1.KmL;
+
+                });
+
+                ex.Message.ShouldEqual("Odometer Error");
+            }
+
+        }
     }
-  }
 }
---
+++ CarFuel.Utilities/SystemTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFuel.Utilities {
+    public static class SystemTime {
+
+        public static Func<DateTime> Now = () => DateTime.Now;
+
+        public static void SetDateTime(DateTime now) {
+            Now = () => now;
+        }
+
+        public static void ResetDateTime() {
+            Now = () => DateTime.Now;
+        }
+
+    }
+}
